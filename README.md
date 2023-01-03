968e056d4f57596be87c588c462dc7ce8213ec7f
Roberto Cardenas <robertocardenas@berkeley.edu>
2023-01-03 13:07:33 -0800

    Nav Bar, Hero Section mobile done

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # fogondemima
-Menu, horarios y contacto del restaurante El Fogon de mi Ma. Maria, ubicado en Rosarito, Baja California.
+Menu, horarios y contacto del restaurante El Fogon de Mi Ma. Maria, ubicado en Rosarito, Baja California.
---
+++ js/observers.js
@@ -0,0 +1,42 @@
+// Select elements
+const header = document.querySelector("header");
+const heroSection = document.querySelector(".hero");
+const menuSection = document.querySelector(".menu");
+
+// Options
+heroSectionOptions = {
+  threshold: "0.95"
+};
+
+sectionOptions = {
+  rootMargin: "-1% 0px -99% 0px"
+};
+
+// Intersection Observers
+const heroSectionObserver = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    if (!entry.isIntersecting) {
+      header.classList.add('nav-scrolled');
+    } else {
+      header.classList.remove('nav-scrolled');
+    }
+  });
+}, heroSectionOptions);
+
+const sectionObserver = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      // Get current section
+      const sectionName = entry.target.getAttribute('data-section');
+
+      // Turn off the previous active tab and turn on the current active one
+      header.getElementsByClassName('active')[0].classList.remove('active');
+      header.getElementsByClassName(sectionName)[0].classList.add('active');
+    }
+  });
+}, sectionOptions);
+
+heroSectionObserver.observe(heroSection);
+document.querySelectorAll('section').forEach((section) => {
+  sectionObserver.observe(section);
+});
